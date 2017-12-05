f4fa43f742eae2898ae23a9dbb1d136905ba3ffb
Peter Roberts <peter.roberts@gmail.com>
2017-12-05 18:44:50 +0000

    Change tabs to spaces, remove inconsistent values

--- playerscroll.py
+++ playerscroll.py
@@ -2,16 +2,19 @@ import math, random, sys
 import pygame
 from pygame.locals import *
 
+
 # exit the program
 def events():
-	for event in pygame.event.get():
-		if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
-			pygame.quit()
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN
+                                  and event.key == K_ESCAPE):
+            pygame.quit()
+            sys.exit()
 
-# define display surface			
-W=1280
-H=700
+
+# define display surface
+W = 1280
+H = 700
 HW = W / 2
 U = H / 2
 AREA = W * H
@@ -30,8 +33,7 @@ WHITE = (255, 255, 255, 255)
 bg = pygame.image.load("RESIZED 1820_1024.jpg").convert()
 bgWidth, bgHeight = bg.get_rect().size
 
-
-stageWidth = bgWidth 
+stageWidth = bgWidth
 stagePosX = 0
 
 stageHeight = 900
@@ -52,60 +54,65 @@ playerVelocityY = 0
 
 # main loop
 while True:
-	events()
+    events()
 
-	k = pygame.key.get_pressed()
-	
-	if k[K_RIGHT]:
-		playerVelocityX = 5
-	elif k[K_LEFT]:
-		playerVelocityX = -5
-	elif k[K_UP]:
-		playerVelocityY = -5
-	elif k[K_DOWN]:
-		playerVelocityY = 5
-	else:
-		playerVelocityX = 0
-		playerVelocityY = 0
-		
-	playerPosX += playerVelocityX
-	playerPosY += playerVelocityY
-	
-	if playerPosX > stageWidth - circleRadius: playerPosX = stageWidth - circleRadius 
-	if playerPosX < circleRadius: playerPosX = circleRadius 
-	if playerPosX < startScrollingPosX: circlePosX = playerPosX 
-	elif playerPosX > stageWidth - startScrollingPosX: circlePosX = playerPosX - stageWidth + W
-	else:
-		circlePosX = startScrollingPosX
-		stagePosX += -playerVelocityX
-		
-	if playerPosY > (stageHeight) - circleRadius_1: playerPosY = stageHeight - circleRadius_1 
-	if playerPosY < circleRadius_1: playerPosY = circleRadius_1 
-	if playerPosY < startScrollingPosY: circlePosY = playerPosY 
-	elif playerPosY > stageHeight - startScrollingPosY: circlePosY = playerPosY - stageHeight + H
-	else:
-		circlePosY = startScrollingPosY
-		stagePosY += -playerVelocityY
-		
-	rel_x = stagePosX % bgWidth
-	rel_y = stagePosY % bgHeight
-	DS.blit(bg,(rel_x - bgWidth, rel_y - bgHeight))
-	
-        if rel_y < H:
+    k = pygame.key.get_pressed()
 
-                DS.blit(bg, (0, rel_y))
+    if k[K_RIGHT]:
+        playerVelocityX = 5
+    elif k[K_LEFT]:
+        playerVelocityX = -5
+    elif k[K_UP]:
+        playerVelocityY = -5
+    elif k[K_DOWN]:
+        playerVelocityY = 5
+    else:
+        playerVelocityX = 0
+        playerVelocityY = 0
 
-	if rel_x < W:
-                DS.blit(bg, (rel_x, 0))
-	
-	
-	
+    playerPosX += playerVelocityX
+    playerPosY += playerVelocityY
 
+    if playerPosX > (stageWidth - circleRadius):
+        playerPosX = stageWidth - circleRadius
 
+    if playerPosX < circleRadius:
+        playerPosX = circleRadius
+    if playerPosX < startScrollingPosX:
+        circlePosX = playerPosX
+    elif playerPosX > stageWidth - startScrollingPosX:
+        circlePosX = playerPosX - stageWidth + W
+    else:
+        circlePosX = startScrollingPosX
+        stagePosX += -playerVelocityX
 
-	pygame.draw.circle(DS, WHITE, (int(circlePosX), playerPosY - 25), circleRadius, 0)
-	pygame.draw.circle(DS, WHITE, (int(circlePosX), playerPosY - 25), circleRadius_1, 0)
+    if playerPosY > (stageHeight) - circleRadius_1:
+        playerPosY = stageHeight - circleRadius_1
+    if playerPosY < circleRadius_1:
+        playerPosY = circleRadius_1
+    if playerPosY < startScrollingPosY:
+        circlePosY = playerPosY
+    elif playerPosY > stageHeight - startScrollingPosY:
+        circlePosY = playerPosY - stageHeight + H
+    else:
+        circlePosY = startScrollingPosY
+        stagePosY += -playerVelocityY
 
-	pygame.display.update()
-	CLOCK.tick(FPS)
-	DS.fill(BLACK)
+    rel_x = stagePosX % bgWidth
+    rel_y = stagePosY % bgHeight
+    DS.blit(bg, (rel_x - bgWidth, rel_y - bgHeight))
+
+    if rel_y < H:
+        DS.blit(bg, (0, rel_y))
+
+    if rel_x < W:
+        DS.blit(bg, (rel_x, 0))
+
+    pygame.draw.circle(DS, WHITE, (int(circlePosX), playerPosY - 25),
+                       circleRadius, 0)
+    pygame.draw.circle(DS, WHITE, (int(circlePosX), playerPosY - 25),
+                       circleRadius_1, 0)
+
+    pygame.display.update()
+    CLOCK.tick(FPS)
+    DS.fill(BLACK)
